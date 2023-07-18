a750f37195f2144dae4c8b66fc5c1623b5d5a3b2
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2023-07-18 16:43:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -68,3 +68,88 @@ app.listen(3000, function() {
   console.log("Server started on port 3000"); 
 });
 
+.................................................................................................................................................
+Next attempt 
+
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+let homeContent ="Lets complete Challenge 2";
+let aboutContent = "Here's your About Content :)";
+let contactContent = "Here's your Contacts Content :)";
+
+const app = express();
+
+var posts = [];
+
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+
+app.get("/" , function(req,res){
+    res.render("home", {
+        startingContent:homeContent,
+        posts:posts
+    });
+    
+})
+
+app.get("/about" , function(req,res){
+    res.render("about", {about:aboutContent});
+})
+app.get("/contact" , function(req,res){
+    res.render("contact", {contact:contactContent});
+})
+
+app.get("/compose" , function(req,res){
+    res.render("compose");
+})
+
+
+
+
+app.post("/compose", function(req,res){
+   
+    const post = {
+        title: req.body.postTitle,
+        content:req.body.postBody
+    };
+  
+    posts.push(post);
+
+    res.redirect("/");
+
+})
+
+app.get("/posts/:postName",function( req, res){
+
+    const requestedTitle = _.lowerCase(req.params.postName);
+
+    posts.forEach(function(post){
+        const storedTitle = _.lowerCase(post.title);
+
+        // if(storedTitle === requestedTitle)
+        // {console.log("Match found !!");}
+        // else
+        // {console.log("Match not found");}
+
+        if(storedTitle === requestedTitle)
+         {
+             res.render("post",{
+                title:post.title,
+                content: post.content   })
+         }
+    });
+
+});
+
+app.listen("3000" , function(req,res){
+    console.log("Server started on the port 3000 !!");
+})
+
